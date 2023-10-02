ff7f5a73f1e2808c95f6e672f6faba85d806dfe1
MicaViera <vieranoelia98@gmail.com>
2023-10-02 02:00:50 +0000

    Write a class Square that defines a square.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Write a class Square that defines a square"""
+
+
 class Square:
     """Square Class"""
+    __size = 0
     def __init__(self, size):
         """Initializing"""
         self.__size = size
