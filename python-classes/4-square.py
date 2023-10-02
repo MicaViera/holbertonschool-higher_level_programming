ac6e15abff33000024f0a3d1dac97dfc336da3cc
MicaViera <vieranoelia98@gmail.com>
2023-10-02 02:52:29 +0000

    Write a class Square that defines a square, properties and area.

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initializing Square Function"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area of a Square Function"""
+        area = self.__size * self.__size
+        return area
