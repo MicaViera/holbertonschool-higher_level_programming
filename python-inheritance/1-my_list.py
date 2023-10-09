e6249d8b5bc2379393e777dbaa6f01d11297e3de
MicaViera <vieranoelia98@gmail.com>
2023-10-09 00:35:03 +0000

    Write a class MyList that inherits from list.

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list."""
+
+
+class MyList:
+    """My List Class."""
+
+    def print_sorted(self):
+        """Function that prints a list."""
+        print(sorted(self))
