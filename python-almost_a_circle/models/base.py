d91d1058a61747ff35909e02c2b3bce953c46944
MicaViera <vieranoelia98@gmail.com>
2023-10-10 18:50:26 +0000

    Function with class Base.

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write the first Class Base."""
+
+
+class Base:
+    """Base Class."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Function that Initializes the class."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
