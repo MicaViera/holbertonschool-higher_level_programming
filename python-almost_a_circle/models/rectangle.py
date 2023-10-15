c13e7e25f3e526a18313cb9b38f454d9200e464e
MicaViera <vieranoelia98@gmail.com>
2023-10-15 00:52:36 +0000

    Update Rectangle class: update *args.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,17 @@ class Rectangle(Base):
         """Function that returns a string."""
         return f"[Rectangle] ({self.id}) {self.__x}\
 /{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """Method that assigns a argument to each attribute."""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
