9e204973c0f4b068a335c23ce1457e68ecd83185
MicaViera <vieranoelia98@gmail.com>
2023-11-14 10:17:47 -0800

    Fixing class Student and a method to serializate the class.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,7 +10,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Method that retrieves a dictionary representation."""
         return self.__dict__
