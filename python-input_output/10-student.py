6c070fc3fcc7c03031b8a9307c6ebb19fa398776
MicaViera <vieranoelia98@gmail.com>
2023-11-14 11:07:58 -0800

    Student class with json serialization method.

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a class Student."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Method that initializes the class."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method that retrieves a dictionary representation."""
+        if isinstance(attrs, list):
+            dictionary = {}
+            for item in attrs:
+                if item in self.__dict__:
+                    dictionary[item] = self.__dict__[item]
+            return dictionary
+        else:
+            return self.__dict__
