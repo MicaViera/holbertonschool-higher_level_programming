4702d4e33e75e6cece45b1537be7a1b8dbe30562
MicaViera <vieranoelia98@gmail.com>
2023-12-01 06:43:03 -0800

    Script that displays the values of the table that matches the name but safe from SQL injections.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that displays the list of values that matches the name,
+safe from MySQL injections."""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_password,
+        database=database_name
+    )
+
+    database_cursor = database.cursor()
+    database_cursor.execute(
+        "SELECT * FROM states "
+        "WHERE BINARY name LIKE %s "
+        "ORDER BY states.id ASC", 
+        (argv[4],))
+
+    for row in database_cursor.fetchall():
+        print(row)
+
+    database_cursor.close()
+    database.close()
