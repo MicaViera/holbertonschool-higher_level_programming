6f45b6cf55244ede7d13e3a10dbf8d2b86efb223
MicaViera <vieranoelia98@gmail.com>
2023-12-11 03:38:04 +0000

    Model city that inherits from Base model.

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""File that contains te class definition of a City and an instance."""
+from sqlalchemy import Column, Integer, String
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class."""
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey(State.id))
